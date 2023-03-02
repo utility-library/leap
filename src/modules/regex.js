9fa0d4c888ca0a42e5ba28975c86db96f5ac18fc
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-03-02 01:32:04 +0100

    fix(regex): MatchRegex made usable + MatchAllRegex fixed

--- src/modules/regex.js
+++ src/modules/regex.js
@@ -1,11 +1,24 @@
-function MatchAllRegex(string, pattern) {
-    return Array.from(string.matchAll(pattern))
+function MatchAllRegex(string, regex) {
+    let m
+    let result = []
+
+    while ((m = regex.exec(string)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++
+        }
+        
+        // The result can be accessed through the `m`-variable.
+        result.push(m)
+    }
+
+    return result
 }
 
-function MatchRegex(pattern, text) {
+function MatchRegex(string, pattern) {
     let regex = new RegExp(pattern)
 
-    return regex.exec(text)
+    return regex.exec(string)
 }
 
 export {MatchRegex, MatchAllRegex}
