7fec21502f19652fbb50ce6514f10615e2a381b9
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-03-07 20:52:18 +0100

    refactor(postProcessing): removed "write" parameter in place of type == build

--- src/modules/postProcessing.js
+++ src/modules/postProcessing.js
@@ -3,16 +3,6 @@ import glob from "glob"
 import fs from "fs"
 import {Features} from "../index"
 
-let validWindowsPath = VerEx()
-    .startOfLine()
-    .range('a', 'z', 'A', 'Z')
-    .find(":/")
-    .beginCapture()
-        .anythingBut("/")
-        .then("/")
-    .endCapture()
-    .oneOrMore()
-
 function ResolveFile(resourcePath, file) {
     if (file.includes("*") > 0) { // If have some glob
         return glob.sync(file, {cwd: resourcePath, absolute: true})
@@ -22,13 +12,7 @@ function ResolveFile(resourcePath, file) {
 }
 
 function PostProcess(resourceName, file, type, write = true) {
-    let fileData = undefined;
-
-    if (validWindowsPath.test(file)) {
-        fileData = fs.readFileSync(file, "utf-8")
-    } else {
-        fileData = file
-    }
+    let fileData = (type == "build") ? fs.readFileSync(file, "utf-8") : file;
 
     for (let feature of Features) {
         if (typeof feature.from == "string") {
@@ -48,16 +32,12 @@ function PostProcess(resourceName, file, type, write = true) {
         }
     }
 
-    if (write) {
-        if (type == "build") {
-            let outputFileDir = file.replace(resourceName, resourceName+"/build") // Add "/build" after the resource name
-            let outputDir = outputFileDir.replace(VerEx().word().then(".lua"), "") // Remove "filename.lua"
-        
-            fs.mkdirSync(outputDir, {recursive: true})
-            fs.writeFileSync(outputFileDir, fileData)
-        } else {
-            fs.writeFileSync(file, fileData) // Temp overwrite the file
-        }
+    if (type == "build") {
+        let outputFileDir = file.replace(resourceName, resourceName+"/build") // Add "/build" after the resource name
+        let outputDir = outputFileDir.replace(VerEx().word().then(".lua"), "") // Remove "filename.lua"
+    
+        fs.mkdirSync(outputDir, {recursive: true})
+        fs.writeFileSync(outputFileDir, fileData)
     } else {
         return fileData
     }
