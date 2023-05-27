6a94eab076386c3d6622bb695d89b3c328c79e8b
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-05-27 17:15:12 +0200

    feat: instantiated object sharing between client > server and server > client

--- src/features/classes.js
+++ src/features/classes.js
@@ -98,7 +98,7 @@ let Class = {
         
         /* Beautified code, code is minified to help with error debugging
             $1 = function(...)
-                local obj = setmetatable({}, {
+                local obj = setmetatable({__type = "$1"}, {
                     __index = function(self, key) 
                         return Prototype$1[key] 
                     end
@@ -111,14 +111,14 @@ let Class = {
                 return obj
             end
 
-            Prototype$1 = {type = "$1",
+            Prototype$1 = {
         */
         
         if (matchIndices.length > 0) {
             file = classIterator(file, matchIndices)
 
             file = file.replace(classMatch, dedent`
-                $1=function(...)local a=setmetatable({},{__index=function(self,b)return Prototype$1[b]end})if a.constructor then a:constructor(...)end;return a end;Prototype$1={type = "$1",
+                $1=function(...)local a=setmetatable({__type = "$1"},{__index=function(self,b)return Prototype$1[b]end})if a.constructor then a:constructor(...)end;return a end;Prototype$1={
             `)
         }
 
@@ -149,7 +149,7 @@ let ClassExtends = {
                     error("ExtendingNotDefined: trying to extend the class $2 but is not defined")
                 end
 
-                local obj = setmetatable({}, {
+                local obj = setmetatable({__type = "$1"}, {
                     __index = function(self, key) 
                         return Prototype$1[key] or Prototype$2[key] 
                     end
@@ -162,34 +162,93 @@ let ClassExtends = {
                 return obj
             end
 
-            Prototype$1 = {type = "$1",
+            Prototype$1 = {
         */
 
         return file.replace(classExtendsMatch, dedent`
-            $1=function(...)if Prototype$2 then Prototype$1.super=setmetatable({},{__index=function(self,a)return Prototype$2[a]end,__call=function(self,...)self.constructor(...)end})else error("ExtendingNotDefined: trying to extend the class $2 that is not defined")end;local b=setmetatable({},{__index=function(self,a)return Prototype$1[a]or Prototype$2[a]end})if b.constructor then b:constructor(...)end;return b end;Prototype$1={type = "$1",
+            $1=function(...)if Prototype$2 then Prototype$1.super=setmetatable({},{__index=function(self,a)return Prototype$2[a]end,__call=function(self,...)self.constructor(...)end})else error("ExtendingNotDefined: trying to extend the class $2 that is not defined")end;local b=setmetatable({__type = "$1"},{__index=function(self,a)return Prototype$1[a]or Prototype$2[a]end})if b.constructor then b:constructor(...)end;return b end;Prototype$1={
         `)
     }
 }
 
+//#region Preservation of the class type 
 
 /* Beautified code, code is minified to help with error debugging
-
-    -- Type function wrapper to add classes type (is here in order not to add a line to the beginning of the file and thus shred the stack traceback lines)
-    -- We check to see if there is already a class that has overridden the type function so that there is no stack overflow
+    -- Type function wrapper to add classes type
     
-    if not _type then
-        _type = type
-        type = function(var)
-            local realType = _type(var)
+    _type = type -- we preserve the original "type" function
+    type = function(var)
+        local realType = _type(var)
 
-            if realType == "table" and var.type then
-                return var.type
-            else
-                return realType
-            end
+        if realType == "table" and var.__type then
+            return var.__type
+        else
+            return realType
         end
     end
 */
-AddHook(["class", "classExtends"], 'if not _type then _type=type;type=function(b)local realType=_type(b)if realType=="table"and b.type then return b.type else return realType end end end')
+AddHook(["class", "classExtends"], '_type=type;type=function(b)local realType=_type(b)if realType=="table"and b.__type then return b.__type else return realType end end')
+
+//#endregion
+
+//#region Sharing of instantiated objects between server > client and client > server
+
+/* Beautified code, code is minified to help with error debugging
+    local __reconvertToInstantiatedObject = function(params)    
+        for k,v in ipairs(params) do
+            if _type(v) == "table" and v.__type then -- if the real type its a table and have the __type variable (its an instantiated object)
+                local class = _G[v.__type] -- get the class from current side (client/server)
+
+                if class then -- if the class exist, recreate it from the insantiated object
+                    params[k] = class()
+                    
+                    for _k, _v in pairs(v) do -- ovverride variables in the instantiated object
+                        params[k][_k] = _v
+                    end
+                end
+            end
+        end
+
+        return params
+    end
+
+    -- also works with RegisterServerEvent (https://github.com/citizenfx/fivem/blob/master/data/shared/citizen/scripting/lua/scheduler.lua#L358)
+    local _RegisterNetEvent = RegisterNetEvent
+    RegisterNetEvent = function(name, func)
+        if name then
+            if func then
+                _RegisterNetEvent(name, function(...)
+                    local params = {...}
+
+                    if next(params) ~= nil then
+                        params = __reconvertToInstantiatedObject(params)
+                    end
+                    
+                    func(table.unpack(params))
+                end)
+            else
+                _RegisterNetEvent(name)
+            end
+        end
+    end
+
+    local _AddEventHandler = AddEventHandler
+    AddEventHandler = function(name, func)
+        if name and func then
+            _AddEventHandler(name, function(...)
+                local params = {...}
+
+                if next(params) ~= nil then
+                    params = __reconvertToInstantiatedObject(params)
+                end
+
+                func(table.unpack(params))
+            end)
+        end
+    end
+*/
+AddHook(["class", "classExtends"], 'local a=function(b)for c,d in ipairs(b)do if _type(d)=="table"and d.__type then local e=_G[d.__type]if e then b[c]=e()for f,g in pairs(d)do b[c][f]=g end end end end;return b end;local h=RegisterNetEvent;RegisterNetEvent=function(i,j)if i then if j then h(i,function(...)local b={...}if next(b)~=nil then b=a(b)end;j(table.unpack(b))end)else h(i)end end end;local k=AddEventHandler;AddEventHandler=function(i,j)if i and j then k(i,function(...)local b={...}if next(b)~=nil then b=a(b)end;j(table.unpack(b))end)end end')
+
+//#endregion
 
 export {Class, ClassExtends}
