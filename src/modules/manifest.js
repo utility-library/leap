221876ae859aa9da9724ecb954b94462df6be114
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-07-25 15:48:51 +0200

    fix(manifest): skip imports from manifest when building

--- src/modules/manifest.js
+++ src/modules/manifest.js
@@ -1,11 +1,17 @@
-function GetAllResourceMetadata(resourceName, key) {
+function GetAllResourceMetadata(resourceName, key, type) {
     let metadataNum = GetNumResourceMetadata(resourceName, key)
     let result = []
 
     for (let i=0; i < metadataNum; i++) {     
         let metadata = GetResourceMetadata(resourceName, key, i)
-        
+ 
         if (!metadata.includes("--") && metadata.includes(".lua")) {
+            if (type == "build") {
+                if (metadata.includes("@")) {
+                    continue
+                }
+            }
+
             result.push(metadata)
         }
     }
