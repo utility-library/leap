3536b63b2f5f8051bd2f54d1b5f2f23953fb5fc7
MrFreex <filippolissandrin@gmail.com>
2023-08-04 12:08:56 +0200

    fix: arrow functions feature
    GetLine() was returning a 1-based line number, incompatible with the slice function. Subtracting one does the job.

--- src/features/arrowFunction.js
+++ src/features/arrowFunction.js
@@ -4,53 +4,53 @@ import { getLine } from "../modules/linesManipulation";
 import { MatchAllRegex } from "../modules/regex";
 
 let match = VerEx()
-    // with brackets
-    .maybe(VerEx()
-        .find("(")
-        .beginCapture()
-            .anythingBut("()")
-        .endCapture()
-        .then(")")
-    )
-    // without brackets
-    .maybe(VerEx()
-        .beginCapture()
-            .word()
-        .endCapture()
-    )
+	// with brackets
+	.maybe(VerEx().find("(").beginCapture().anythingBut("()").endCapture().then(")"))
+	// without brackets
+	.maybe(VerEx().beginCapture().word().endCapture())
 
-    .maybe(" ")
-    .then("=>")
-    .maybe(" ")
-    .then("{")
+	.maybe(" ")
+	.then("=>")
+	.maybe(" ")
+	.then("{");
 
 let ArrowFunction = {
-    id: "arrowFunction",
-    from: match,
-    to: function(originalFile) {
-        let file = originalFile
-        let matches = MatchAllRegex(file, match);
-        // replace all closing tags from "}" to "end"
-        matches.map(x => {
-            [file] = ReplaceFunctionEnding(file,file.split("\n").slice(getLine(originalFile, x.index)).join("\n"))
+	id: "arrowFunction",
+	from: match,
+	to: function (originalFile) {
+		let file = originalFile;
+		let matches = MatchAllRegex(file, match);
+		// replace all closing tags from "}" to "end"
+		matches.map((x) => {
+			[file] = ReplaceFunctionEnding(
+				file,
+				file
+					.split("\n")
+					.slice(getLine(originalFile, x.index) - 1) // The index of the line was one based... breaking the whole feature
+					.join("\n")
+			);
 
-            match.removeModifier("g") // we want to replace it once, so later, if necessary, we can replace the one without parentheses with the correct capture group
+			//console.log(file);
 
-            if (x[1] != null) { // match with the brackets
-                // replace the opening tag from "(params) => {" to "function(params)"                
-                file = file.replace(match, "function($1)") 
-            } else if (x[2] != null) { // match without the brackets                
-                // replace the opening tag from "param => {" to "function(param)"
-                file = file.replace(match, "function($2)")
-            }
+			match.removeModifier("g"); // we want to replace it once, so later, if necessary, we can replace the one without parentheses with the correct capture group
 
-            match.addModifier("g") // we re-add the flag for upcoming matches
-        })
+			if (x[1] != null) {
+				// match with the brackets
+				// replace the opening tag from "(params) => {" to "function(params)"
+				file = file.replace(match, "function($1)");
+			} else if (x[2] != null) {
+				// match without the brackets
+				// replace the opening tag from "param => {" to "function(param)"
+				file = file.replace(match, "function($2)");
+			}
 
-        //console.log(matches)
-    
-        return file
-    },
-}
+			match.addModifier("g"); // we re-add the flag for upcoming matches
+		});
 
-export {ArrowFunction}
+		//console.log(matches)
+
+		return file;
+	},
+};
+
+export { ArrowFunction };
--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,39 +1,48 @@
-import { getLine } from "./linesManipulation"
-import "./string"
+import { getLine } from "./linesManipulation";
+import "./string";
 
 function ReplaceFunctionEnding(string, linesAfterMatch, to = "end", opening = "{", closing = "}") {
-    let lineByLine = linesAfterMatch.split("\n") // Unpack lines
-    let i
-    let curlyBracesCounter = 0
+	let lineByLine = linesAfterMatch.split("\n"); // Unpack lines
+	let i;
+	let curlyBracesCounter = 0;
 
-    for (i in lineByLine) {
-        let line = lineByLine[i]
-        
-        if (typeof opening == "object") {
-            for (e of opening) {
-                curlyBracesCounter += line.occurrences(e)
-            }
-        } else {
-            curlyBracesCounter += line.occurrences(opening)
-        }
+	if (to === null) to = "end";
+	if (opening === null) opening = "{";
+	if (closing === null) closing = "}";
 
-        curlyBracesCounter -= line.occurrences(closing)
+	//console.log(to, new Error().stack);
 
-        //console.log(i, line)
-        if (curlyBracesCounter == 0) {
-            if (to) {
-                lineByLine[i] = line.replace(closing, to)
-            }
-            break
-        }
-    }
-    
-    string = string.replace(linesAfterMatch, lineByLine.join("\n"))
+	for (i in lineByLine) {
+		let line = lineByLine[i];
 
-    let startLine = getLine(string, lineByLine[0])
-    let endLine = startLine + parseInt(i)
+		if (typeof opening == "object") {
+			for (e of opening) {
+				curlyBracesCounter += line.occurrences(e);
+			}
+		} else {
+			curlyBracesCounter += line.occurrences(opening);
+		}
 
-    return [string, startLine, endLine]
+		curlyBracesCounter -= line.occurrences(closing);
+
+		//console.log(i, line)
+		if (curlyBracesCounter == 0) {
+			//console.log(to, line);
+			if (to) {
+				lineByLine[i] = line.replace(closing, to);
+			}
+
+			break;
+		}
+	}
+
+	//console.log(linesAfterMatch.includes(lineByLine.join("\n")));
+	string = string.replace(linesAfterMatch, lineByLine.join("\n"));
+
+	let startLine = getLine(string, lineByLine[0]);
+	let endLine = startLine + parseInt(i);
+
+	return [string, startLine, endLine];
 }
 
-export {ReplaceFunctionEnding}
+export { ReplaceFunctionEnding };
