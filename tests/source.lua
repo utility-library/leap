f35c64eea6569a5ddf3598f676a98760b5af3773
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-10-22 03:24:44 +0200

    tests: updated tests for the arrow function

--- tests/source.lua
+++ tests/source.lua
@@ -1,22 +1,3 @@
-class Dio {
-    value = "dio",
-
-    destructor = function()
-        print("destructor", self.value)
-    end
-}
-
-class Test extends Dio {
-    value = "test",
-
-    constructor = function(param <string>)
-        print("constructor", self.value)
-        print("param", param)
-    end,
-
-    destructor = function()
-        print("destructor", self.value)
-    end
-}
-
-local test = new Test("test")
+Citizen.CreateThread((a, b) => { 
+    print("hello")
+})
