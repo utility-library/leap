59e8182c0c203043fbae5b97e1a079b03071e9e7
XenoS-ITA <arcobalenosixsiegea@gmail.com>
2025-06-07 23:40:05 +0200

    Replace leap content fully with leap3 snapshot

--- config.js
+++
@@ -1,7 +0,0 @@
-var Config = {}
-
-Config.Dev = GetConvarInt("leap:dev", 0) == 1 ? true : false
-
-
-// ignore
-export {Config}
--- src/features/notEqual.js
+++
@@ -1,7 +0,0 @@
-let NotEqual = {
-    id: "notEqual",
-    from: "!=",
-    to: "~="
-}
-
-export {NotEqual}
